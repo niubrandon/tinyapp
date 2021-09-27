e339e4cfc87cf84c5a8efc18584622672f768f1c
niubrandon <niubrandon@gmail.com>
2021-09-27 14:52:41 -0400

    added urls_index.ejs and hello_world.ejs

--- express_server.js
+++ express_server.js
@@ -16,13 +16,21 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  const templateVars = {greeting: 'Hello World!'};
+  res.render("hello_world", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res)  => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.get("/set", (req, res) => {
   const a = 1;
